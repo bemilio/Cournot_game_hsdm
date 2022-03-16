0f4faff29e751d3995a7c390509e1f5b913137ff
Emilio Benenati <e.benenati@tudelft.nl>
2022-03-16 19:41:40 +0100

    reduced required cores in python job

--- run_job_python.sh
+++ run_job_python.sh
@@ -1,5 +1,5 @@
 # Request one node with n free processor cores
-#PBS -l nodes=1:ppn=4:typel
+#PBS -l nodes=1:ppn=1:typel
 # Mail me when the job ends for any reason
 #PBS -m ae
 #PBS -M e.benenati@tudelft.nl
