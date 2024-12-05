db1185eed333f63e7d69f72d17e61bc7e053c309
Emilio Benenati <e.benenati@tudelft.nl>
2024-12-05 14:51:26 +0100

    reverted to CDC version

--- plot.py
+++ plot.py
@@ -11,15 +11,15 @@ plt.rcParams.update({
     "font.serif": ["Palatino"],
     "font.size": 14
 })
-import networkx as nx
+mpl.rcParams['grid.linewidth']=0.5
+# import networkx as nx
 import numpy as np
 import pickle
-from scipy import sparse
+# from scipy import sparse
 
 check_test = False
 
-# directory = '/Users/ebenenati/Repositories/Results_hsdm_ppp_academic/Statistics_16_mar/'
-directory='/Users/ebenenati/surfdrive/TUDelft/Simulations/PPP_hsdm_academic/Statistics_multiple_beta/'
+directory='Data/'
 x_hsdm_all=[]
 x_not_hsdm_all=[]
 residual_hsdm_all=[]
@@ -54,7 +54,7 @@ for filename in os.listdir(directory):
 #     sigma_computed_not_hsdm = sigma_computed_not_hsdm + x_not_hsdm[(agent_id, test)]
 # print('For hsdm The difference between computed and saved sigma is: ', np.linalg.norm(sigma_computed_not_hsdm - sigma_not_hsdm))
 
-hsdm_exponent_to_plot = 1
+hsdm_exponent_to_plot = 0.6
 if hsdm_exponent_to_plot in tested_hsdm_exponents:
     index_exponent_to_plot = tested_hsdm_exponents.index(hsdm_exponent_to_plot)
 else:
@@ -80,10 +80,9 @@ for problem in range(N_problems):
         for index_agent in range(N_agents):
             stacked_x_hsdm = np.vstack(( stacked_x_hsdm, x_hsdm_all[problem][(index_agent, index_exponent_to_plot, index_init_state )] )) if stacked_x_hsdm.size else x_hsdm_all[problem][(index_agent, index_exponent_to_plot, index_init_state )] 
             stacked_x_not_hsdm = np.vstack( ( stacked_x_not_hsdm, x_not_hsdm_all[problem][(index_agent, index_init_state )] ) ) if stacked_x_not_hsdm.size else x_not_hsdm_all[problem][(index_agent, index_init_state )]
-        distance_optimizers[problem, index_init_state] = np.linalg.norm(stacked_x_hsdm  - stacked_x_not_hsdm, inf) 
+        distance_optimizers[problem, index_init_state] = np.linalg.norm(stacked_x_hsdm  - stacked_x_not_hsdm, inf)/ np.linalg.norm(stacked_x_hsdm, inf)
 
-## Plot residual for all exponents
-
+## Plot residual for a particular exponent
 len_residual = residual_hsdm_all[0][(0,0)].shape[0]
 residual_hsdm_stacked={}
 for i in range(N_exponents_hsdm):
@@ -110,27 +109,7 @@ x_total_hsdm = np.matrix([])
 N=len(x_hsdm)
 
 fig, ax = plt.subplots(figsize=(6, 2.5), layout='constrained') 
-N_iter = (average_residual_hsdm.shape[1]-1) * 20
-colors = ["#e377c2", "#2ca02c", "#1f77b4"]
-for i_exponent in range(N_exponents_hsdm):
-    average_residual_hsdm = np.mean(residual_hsdm_stacked[tested_hsdm_exponents[i_exponent]], axis =0)
-    ax.loglog(np.arange(0, N_iter,20), np.ravel(average_residual_hsdm[:,:-1]), color=colors[i_exponent], label=tested_hsdm_exponents[i_exponent])
-ax.loglog(np.arange(0, N_iter,20), np.ravel(average_residual_not_hsdm[:,:-1]), color="#ff7f0e", label="PPP")
-# ax.fill_between(np.arange(0, N_iter,20), np.ravel(min_residual_hsdm[:,:-1]), np.ravel(max_residual_hsdm[:,:-1]), alpha=0.2)
-# ax.fill_between(np.arange(0, N_iter,20), np.ravel(min_residual_not_hsdm[:,:-1]), np.ravel(max_residual_not_hsdm[:,:-1]), alpha=0.2)
-plt.xlabel('Iteration')
-plt.ylabel('Residual')
-plt.ylim((10**(-8), 10**(1)))
-plt.xlim((10**(1), 5*10**(4)))
-plt.grid()
-# plt.title('Residual')
-plt.legend(loc="lower left", fontsize=12)
-plt.savefig('Residual_exponents.pdf')  
 
-## Plot residual for a particular exponent
-
-
-fig, ax = plt.subplots(figsize=(6, 2.5), layout='constrained') 
 N_iter = (average_residual_hsdm.shape[1]-1) * 20
 ax.loglog(np.arange(0, N_iter,20), np.ravel(average_residual_hsdm[:,:-1]), color="#1f77b4", label="Algorithm 1")
 ax.loglog(np.arange(0, N_iter,20), np.ravel(average_residual_not_hsdm[:,:-1]), color="#ff7f0e", label="PPP")
@@ -139,16 +118,32 @@ ax.fill_between(np.arange(0, N_iter,20), np.ravel(min_residual_not_hsdm[:,:-1]),
 plt.xlabel('Iteration')
 plt.ylabel('Residual')
 plt.ylim((10**(-8), 10**(1)))
-plt.xlim((10**(1), 5*10**(4)))
+plt.xlim((10**(1), 10*10**(4)))
 plt.grid()
 
 # plt.title('Residual')
 plt.legend(loc="lower left", fontsize=12)
-plt.savefig('Residual_exponents.pdf') 
+plt.savefig('Residual.pdf') 
+
+## Plot residual for all exponents
 
 
-
-
+fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True, figsize=(6, 5), layout='constrained')
+N_iter = (average_residual_hsdm.shape[1]-1) * 20
+colors = ["#e377c2", "#2ca02c", "#1f77b4"]
+for i_exponent in range(N_exponents_hsdm):
+    average_residual_hsdm = np.mean(residual_hsdm_stacked[tested_hsdm_exponents[i_exponent]], axis =0)
+    ax1.loglog(np.arange(0, N_iter,20), np.ravel(average_residual_hsdm[:,:-1]), linewidth = 2, color=colors[i_exponent], label=r'$\gamma={}$'.format(tested_hsdm_exponents[i_exponent]))
+ax1.loglog(np.arange(0, N_iter,20), np.ravel(average_residual_not_hsdm[:,:-1]), linewidth = 2,color="#ff7f0e", label="PPP")
+# ax.fill_between(np.arange(0, N_iter,20), np.ravel(min_residual_hsdm[:,:-1]), np.ravel(max_residual_hsdm[:,:-1]), alpha=0.2)
+# ax.fill_between(np.arange(0, N_iter,20), np.ravel(min_residual_not_hsdm[:,:-1]), np.ravel(max_residual_not_hsdm[:,:-1]), alpha=0.2)
+plt.xlabel('Iteration')
+ax1.set(ylabel=r'$r(\omega)$')
+ax1.set(ylim=(10**(-8), 10**(1)) )
+plt.xlim((10**(1), 10*10**(4)))
+ax1.grid(which="both")
+# plt.title('Residual')
+ax1.legend(loc="lower left", fontsize=12, ncol=2)
 
 ## Plot cost for all exponents
 
@@ -171,80 +166,34 @@ average_cost_not_hsdm = np.mean(cost_not_hsdm_stacked, axis =0)
 x_total_hsdm = np.matrix([])
 N=len(x_hsdm)
 
-fig, ax = plt.subplots(figsize=(6, 2.5), layout='constrained') 
 N_iter = (average_residual_hsdm.shape[1]-1) * 20
 colors = ["#e377c2", "#2ca02c", "#1f77b4"]
+average_cost_hsdm =[]
 for i_exponent in range(N_exponents_hsdm):
-    average_cost_hsdm = np.mean(cost_hsdm_stacked[tested_hsdm_exponents[i_exponent]], axis =0)
-    ax.plot(np.arange(0, N_iter,20), np.ravel(average_cost_hsdm[:,:-1]), color=colors[i_exponent], label=tested_hsdm_exponents[i_exponent])
-ax.loglog(np.arange(0, N_iter,20), np.ravel(average_cost_not_hsdm[:,:-1]), color="#ff7f0e", label="PPP")
+    average_cost_hsdm.append(np.mean(cost_hsdm_stacked[tested_hsdm_exponents[i_exponent]], axis =0))
+# compare cost to best estimate of optimal cost
+cost_reference = min([ cost[0, -2] for cost in average_cost_hsdm ])
+for i_exponent in range(N_exponents_hsdm):
+    ax2.step(np.arange(0, N_iter,20), np.abs(np.ravel(average_cost_hsdm[i_exponent][:,:-1]- cost_reference)) , linewidth = 2, color=colors[i_exponent], label=r'$\gamma={}$'.format(tested_hsdm_exponents[i_exponent]))
+
+ax2.step(np.arange(0, N_iter,20), np.abs(np.ravel(average_cost_not_hsdm[:,:-1]) - cost_reference), linewidth = 2, color="#ff7f0e", label="PPP")
+ax2.set_xscale('log')
+# ax2.set_yscale('log')
+# ax2.set_yscale('symlog', linthresh=0.05)
 # ax.fill_between(np.arange(0, N_iter,20), np.ravel(min_residual_hsdm[:,:-1]), np.ravel(max_residual_hsdm[:,:-1]), alpha=0.2)
 # ax.fill_between(np.arange(0, N_iter,20), np.ravel(min_residual_not_hsdm[:,:-1]), np.ravel(max_residual_not_hsdm[:,:-1]), alpha=0.2)
-plt.xlabel('Iteration')
-plt.ylabel('Cost')
-# plt.ylim((10**(-8), 10**(1)))
-# plt.xlim((10**(1), 5*10**(4)))
-plt.grid()
+
+plt.ylim((10**(-7), 10**(1)))
+plt.xlim((10**(1), 10*10**(4)))
+# ax2.set_yticks([])
+# ax2.minorticks_off() #Needed to remove ticks that matplotlib sets by its own will
+# ax2.set_yticks([ -10, -1, 0 , 1, 10], [ -10, -1, 0 , 1, 10])
+ax2.set(ylabel='$\|\phi(x) - \phi^{\star}\|$')
+ax2.grid(which="both")
 # plt.title('Residual')
-plt.legend(loc="lower left", fontsize=12)
-plt.savefig('Cost_exponents.pdf')  
+plt.savefig('Comparison_exponents.pdf')  
+plt.savefig('Comparison_exponents.png', dpi=300)  
 
-
-
-
-# sigma_not_electric_hsdm=[]
-# sigma_not_electric_not_hsdm=[]
-# test = 0
-
-# cost_advantage = []
-# distance_from_ppp = []
-# for test in range(N_random_tests):
-#     x_total_hsdm = np.matrix([])
-#     for i in range(N_agents):
-#         x_total_hsdm=np.vstack((x_total_hsdm, x_hsdm[(i, test)])) if x_total_hsdm.size else x_hsdm[(i, test)]
-#     x_total_not_hsdm = np.matrix([])
-#     for i in range(N_agents):
-#         x_total_not_hsdm=np.vstack((x_total_not_hsdm, x_not_hsdm[(i , test)])) if x_total_not_hsdm.size else x_not_hsdm[(i, test)]
-#     distance_from_ppp.append(np.linalg.norm(x_total_hsdm-x_total_not_hsdm, inf))
-#     if cost_not_hsdm[test]> 0.01:
-#         cost_advantage.append( (cost_not_hsdm[test] - cost_hsdm[test])  / cost_not_hsdm[test])
-#     else:
-#         cost_advantage.append(0)
-# fig, ax = plt.subplots(figsize=(5, 1.8), layout='constrained') 
-# a= round(min(distance_from_ppp), 2)
-# b= round(max(distance_from_ppp)+0.01, 2)
-# step=(b-a)/10
-# bins = np.arange(a, b+step, step)
-# plt.hist(distance_from_ppp,rwidth=0.5, bins=bins)
-# plt.xticks(bins)
-# plt.xlabel("$\|x_{HSDM}-x_{PPP}\|_{\infty}$")
-# plt.ylabel("Occurrencies")
-# ax.xlim=([a,b+step])
-# ax.set_xticks=bins
-# ax.set_xticklabels=bins
-# plt.show()
-# plt.title("Distance between HSDM and PPP")
-
-# print("The distance between HSDM and OSQP solution is, ", np.linalg.norm(x_total_hsdm - x_osqp, inf))
-# print("The distance between PPP and OSQP solution is, ", np.linalg.norm(x_total_not_hsdm - x_osqp, inf))
-# print("The distance between PPP and HSDM solution is, ", np.linalg.norm(x_total_not_hsdm - x_total_hsdm, inf))
-
-
-
-# fig, ax = plt.subplots(figsize=(5, 1.8), layout='constrained') 
-# cost_advantage_percentage=[ cost.item()*100 for cost in cost_advantage]
-# a= round(min(cost_advantage_percentage))
-# b= round(max(cost_advantage_percentage)+1)
-# step=(b-a)/10
-# bins = np.arange(0, b+step, step)
-# plt.hist( cost_advantage_percentage, rwidth=0.5, bins=bins)
-# plt.xlabel("Selection function value decrease ($\%$)")
-# plt.ylabel("Occurrencies")
-# ax.xlim=([a,b+step])
-# # ax.set_xticks=bins
-# plt.xticks(bins)
-# # ax.set_xticklabels=bins
-# plt.title("Advantage of HSDM")
 
 fig, ax = plt.subplots(figsize=(6,5), layout='constrained') 
 avg_improvement=np.zeros((N_problems,1))
@@ -266,14 +215,14 @@ fig, ax = plt.subplots(figsize=(6,5), layout='constrained')
 avg_distance=np.zeros((N_problems,1))
 for problem in range(N_problems):
     avg_distance[problem] = np.median(distance_optimizers[problem, :])
-plt.boxplot(distance_optimizers[sorted_idx].T, sym="", whis =10000, vert=False, medianprops=dict(color='k'))
-plt.barh([i+1 for i in range(N_problems)], np.ravel(avg_distance[sorted_idx]), color="#1f77b4")
+plt.boxplot(100*distance_optimizers[sorted_idx].T, sym="", whis =10000, vert=False, medianprops=dict(color='k'))
+plt.barh([i+1 for i in range(N_problems)], np.ravel(100*avg_distance[sorted_idx]), color="#1f77b4")
 # ax.set_xticklabels=binsplt.title("Advantage of HSDM")
 
 plt.yticks([])
 plt.grid(axis='x')
 plt.ylabel('GNE problem instance')
-plt.xlabel(r'$ \|x_{\mathrm{PPP}}-x^{\star}\|_{\infty}$ ')
-plt.xlim((0, 5))
+plt.xlabel(r'$\frac{\|x_{\mathrm{PPP}} -x^{\star}\|_{\infty} } {\| x_{\mathrm{PPP}} \|_{\infty} }  (\%)$ ')
+plt.xlim((0, 200))
 plt.savefig('Distance.pdf') 
 plt.show(block=True)
